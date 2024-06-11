0c4a8560a461ed99b37fa8b5434cfffb082f1021
JeffersonCorreiax <correia_jefferson@outlook.com>
2024-06-11 00:18:54 -0300

    modal de criacao de propriedade ajustada

--- imobiliaria-web/src/components/CreateRentProperty.jsx
+++ imobiliaria-web/src/components/CreateRentProperty.jsx
@@ -13,7 +13,7 @@ const CreateRentProperty = ({ isVisible, onClose }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formattedName = formatName(name); 
+    const formattedName = formatName(name);
     const formData = new FormData();
     formData.append('name', formattedName);
     formData.append('image', image);
@@ -56,36 +56,41 @@ const CreateRentProperty = ({ isVisible, onClose }) => {
       <div className="modal">
         <button className="closeButton" onClick={onClose}>X</button>
         <form onSubmit={handleSubmit}>
-          <label >
+          <label className='labelCreateRentName'>
             Nome da Propriedade:
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
           </label>
-          <label >
+          <label className='labelCreateRent'>
             <span className='labelModalCreate'>Foto:</span>
-         
-            <input type="file" onChange={(e) => setImage(e.target.files[0])} required />
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label htmlFor="file" className="btn-file">Selecionar Foto</label>
+              <span className="file-name">{image ? image.name : 'Nenhum arquivo selecionado'}</span>
+              <input type="file" id="file" className="input-file" onChange={(e) => setImage(e.target.files[0])} required />
+            </div>
           </label>
-          <label >
-          <span className='labelModalCreate'> CEP:</span>
-            <input type="text" value={cep} onChange={handleCepChange} required/>
+          <label className='labelCreateRent' >
+            <span > CEP:</span>
+            <input type="text" value={cep} onChange={handleCepChange} required />
           </label>
-          <label>
+          <label className='labelCreateRent'>
             Rua:
             <input type="text" value={address.logradouro || ''} readOnly />
           </label>
-          <label>
+          <label className='labelCreateRent'>
             Bairro:
             <input type="text" value={address.bairro || ''} readOnly />
           </label>
-          <label>
+          <label className='labelCreateRent'>
             Cidade:
             <input type="text" value={address.localidade || ''} readOnly />
           </label>
-          <label>
+          <label className='labelCreateRent'>
             Estado:
             <input type="text" value={address.uf || ''} readOnly />
           </label>
-          <button type="submit" className="buttonAdmin" style={{padding : "0.5rem", marginTop : "1rem"}}>Criar Propriedade</button>
+          <div className="button-container">
+            <button type="submit" className="buttonCreateRent" style={{ padding: "0.5rem", marginTop: "1rem" }}>Criar Propriedade</button>
+          </div>
         </form>
       </div>
     </div>
